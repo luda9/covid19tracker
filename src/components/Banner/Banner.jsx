2963cd67533f832847190a526c27545976e223e7
Daniel Santillan <ldms99@hotmail.com>
2022-10-15 17:49:31 -0500

    routes completed

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Banner.module.css";
+
+function Banner() {
+  return (
+    <div className={styles.banner}>
+      {/* <img src={banner} alt="covid-banner" className={styles.image}/> */}
+    </div>
+  )
+}
+
+export default Banner
