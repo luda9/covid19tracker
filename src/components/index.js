ced489b862c92e377b8cb324045597e6d885af95
Daniel Santillan <ldms99@hotmail.com>
2022-10-16 12:28:30 -0500

    tracker section added

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as Banner } from './Banner/Banner';
 export { default as Covidinfo } from './Covidinfo/Covidinfo.jsx';
 export { default as Bar } from './Bar/Bar.jsx';
+export { default as TrackerSection } from './TrackerSection/TrackerSection.jsx';
