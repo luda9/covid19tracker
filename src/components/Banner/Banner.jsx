a1e28672cb23700f585df5d5dfe094bad359c718
Daniel Santillan <ldms99@hotmail.com>
2022-10-16 13:16:28 -0500

    footer and responsive added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,6 @@ import styles from "./Banner.module.css";
 function Banner() {
   return (
     <div className={styles.banner}>
-      {/* <img src={banner} alt="covid-banner" className={styles.image}/> */}
     </div>
   )
 }
